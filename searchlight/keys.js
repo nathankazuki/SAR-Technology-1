856981c5a2e44d6425841df492df76acc9a32af3
nathankazuki <nma11@hotmail.ca>
2020-10-22 17:47:44 -0700

    Update keys.js

--- searchlight/keys.js
+++ searchlight/keys.js
@@ -1,3 +1,8 @@
-var personsKey = 'c8188c5823ff4e8b8b8f40a8a8a4ad0f';
-var helicopterKey = 'e98478e0346b43fdb7f3640d52b8f925';
-var aircraftKey = '0b18e04f290a4d169b4168a81efef47a';
+// var personsKey = 'c8188c5823ff4e8b8b8f40a8a8a4ad0f';
+// var helicopterKey = 'e98478e0346b43fdb7f3640d52b8f925';
+// var aircraftKey = '0b18e04f290a4d169b4168a81efef47a';
+
+// Invalid Test Keys
+var personsKey = 'c8188c5823ff4e8b8b8f40a8a8a4ad0g';
+var helicopterKey = 'e98478e0346b43fdb7f3640d52b8f926';
+var aircraftKey = '0b18e04f290a4d169b4168a81efef47b';
